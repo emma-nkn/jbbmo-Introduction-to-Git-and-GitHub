ef58b6e2337c22fe280466be1d56d6a3f9939ad7
emma-nkn <emmacarene7@gmail.com>
2024-12-19 19:05:59 +0100

    Create projet_final.py
    new file

---
+++ projet_final.py
@@ -0,0 +1,6 @@
+1.	# projet_final
+2.	
+3.	## Input package weight and shipping rate
+4.	weight = float(input("Enter the package weight in kilograms: "))
+5.	rate = float(input("Enter the shipping rate per kilogram: "))
+6.	
